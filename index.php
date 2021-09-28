880052ea80b29de268fdf8f8bb34f942179ee553
cherryyam <cherryyam@protonmail.com>
2021-09-29 07:51:40 +0800

    index.php页面

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<html lang="en">
+    <head>
+        <title>qm-blog</title>
+    </head>
+
+    <body>
+        <?php
+            echo "www.github.com/cherryyam/qm-blog"
+        ?>
+    </body>
+</html>
+
+
