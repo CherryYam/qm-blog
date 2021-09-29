bf4e8d13c3fff9969b6d6db8a14eb1f579739d1f
cherryyam <cherryyam@protonmail.com>
2021-09-29 08:01:35 +0800

    index.php页面

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
     <body>
         <?php
-            echo "<a href='https://www.github.com/cherryyam/qm-blog' target='_blank'>www.github.com/cherryyam/qm-blog</a>"
+            echo "<a href='https://www.github.com/cherryyam/qm-blog' target='_blank'>www.github.com/cherryyam/qm-blog</a>";
         ?>
     </body>
 </html>
